bcc0f657ba4a02fbccf67ee633d5bfb1de9a8e9a
Eievui <14899090+eievui5@users.noreply.github.com>
2022-11-02 17:48:09 -0400

    Add more animation to title

--- src/menus/title.asm
+++ src/menus/title.asm
@@ -48,11 +48,13 @@ xSleepingProtags:
 	incbin "res/ui/title/luvui_sleeping.2bpp"
 	incbin "res/ui/title/aris_sleeping.2bpp"
 	incbin "res/ui/title/campfire.2bpp"
+	incbin "res/ui/title/stars.2bpp"
 .end
 xProtagPalettes:
 	incbin "res/ui/title/luvui_sleeping.pal8", 3
 	incbin "res/ui/title/aris_sleeping.pal8", 3
 	incbin "res/ui/title/campfire.pal8", 3
+	incbin "res/ui/title/stars.pal8", 3
 
 xTitleScreenInit:
 	xor a, a
@@ -119,6 +121,12 @@ xTitleScreenInit:
 	ld [wBGPaletteMask], a
 	ld [wOBJPaletteMask], a
 
+	assert wLuvuiFrameCounter + 1 == wArisFrameCounter
+	ld hl, wLuvuiFrameCounter
+	inc a
+	ld [hli], a
+	ld [hli], a
+
 	ld a, BANK(xLakeMusic)
 	ld de, xLakeMusic
 	call StartSongTrampoline
@@ -139,31 +147,130 @@ POPS
 xTitleScreenRedraw:
 	call Rand
 
-	def LUVUI_POSITION EQUS "97, 86"
-	def ARIS_POSITION EQUS "97, 59"
+	def LUVUI_POSITION EQUS "97, 87"
+	def ARIS_POSITION EQUS "97, 58"
 	def FIRE_POSITION EQUS "91, 72"
+
+	def LUVUI_SPEED EQU 90
+	def ARIS_SPEED EQU 100
+
+	ld a, [hFrameCounter]
+	rra
+	jr nc, .ignoreFrame
+		ld a, [wLuvuiFrameCounter]
+		inc a
+		cp a, LUVUI_SPEED * 2
+		jr nz, :+
+		xor a, a
+:
+		ld [wLuvuiFrameCounter], a
+		ld a, [wArisFrameCounter]
+		inc a
+		cp a, ARIS_SPEED * 2
+		jr nz, :+
+		xor a, a
+:
+		ld [wArisFrameCounter], a
+.ignoreFrame
 
 	lb bc, {LUVUI_POSITION}
 	lb de, 0, 0
+	ld a, [wLuvuiFrameCounter]
+	cp a, LUVUI_SPEED
+	jr c, :+
+	ld d, 4
+:
 	call RenderSimpleSprite
 	lb bc, {LUVUI_POSITION} + 8
 	lb de, 2, 0
+	ld a, [wLuvuiFrameCounter]
+	cp a, LUVUI_SPEED
+	jr c, :+
+	ld d, 6
+:
 	call RenderSimpleSprite
 
 	lb bc, {ARIS_POSITION}
-	lb de, 4, 1
+	lb de, 8, 1
+	ld a, [wArisFrameCounter]
+	cp a, ARIS_SPEED
+	jr nc, :+
+	ld d, 12
+:
 	call RenderSimpleSprite
 	lb bc, {ARIS_POSITION} + 8
-	lb de, 6, 1
+	lb de, 10, 1
+	ld a, [wArisFrameCounter]
+	cp a, ARIS_SPEED
+	jr nc, :+
+	ld d, 14
+:
 	call RenderSimpleSprite
 
+
+	ld hl, xStarTable
+.drawStars
+	ld a, [hli]
+	and a, a
+	jr z, .finishedStars
+	dec a
+	jr z, .solid
+	dec a
+	jr z, .dimFlicker
+	dec a
+	jr z, .flicker
+.twinkle
+	ldh a, [hFrameCounter]
+	add a, [hl]
+	and a, %00110000
+	swap a
+	add a, a
+	cpl
+	inc a
+	add a, 38
+	ld d, a
+	jr .drawStar
+
+.dimFlicker
+	ldh a, [hFrameCounter]
+	add a, [hl]
+	and a, %00100000
+	jr z, .solid
+	ld d, 34
+	jr .drawStar
+
+.flicker
+	ldh a, [hFrameCounter]
+	add a, [hl]
+	and a, %00100000
+	jr z, .solid
+	inc hl
+	inc hl
+	inc hl
+	jr .drawStars
+
+.solid
+	ld d, 32
+.drawStar
+	inc hl
+	ld e, 3
+	ld a, [hli]
+	ld c, a
+	ld a, [hli]
+	ld b, a
+	push hl
+	call RenderSimpleSprite
+	pop hl
+	jr .drawStars
+
+.finishedStars
 	lb bc, {FIRE_POSITION}
 	ld e, 2
 	ldh a, [hFrameCounter]
 	rra
 	rra
 	and a, 3 << 2
-	add a, 8
+	add a, 16
 	ld d, a
 	call RenderSimpleSprite
 	lb bc, {FIRE_POSITION} + 8
@@ -172,7 +279,7 @@ xTitleScreenRedraw:
 	rra
 	rra
 	and a, 3 << 2
-	add a, 10
+	add a, 18
 	ld d, a
 	jp RenderSimpleSprite
 
@@ -221,3 +328,54 @@ xTitleScreenClose:
 	ld [hli], a
 	ld [hl], high(InitDungeon)
 	ret
+
+rsset 1
+def solid rb
+def dim_flicker rb
+def flicker rb
+def twinkle rb
+
+def random = 2834123757
+
+macro next_random ; xorshift32
+  def random ^= random << 13
+  def random ^= random >> 17
+  def random ^= random << 5
+endm
+
+macro star
+	next_random
+	db random % 4 + 1
+	next_random
+	db random & $FF, \1, \2
+endm
+
+xStarTable:
+	star 17, 9
+	star 13, 18
+	star 36, 4
+	star 65, 1
+	star 25, 25
+	star 45, 18
+	star 93, 5
+	star 108, 3
+	star 116, 11
+	star 27, 37
+	star 42, 36
+	star 34, 44
+	star 41, 52
+	star 60, 44
+	star 58, 52
+	star 75, 53
+	star 91, 45
+	star 107, 41
+	star 122, 49
+	star 145, 5
+	star 130, 20
+	star 138, 28
+	star 130, 41
+	db 0
+
+section "Title screen variables", wram0
+wLuvuiFrameCounter: db
+wArisFrameCounter: db
