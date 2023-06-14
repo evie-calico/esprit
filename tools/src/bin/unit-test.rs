0c6244310092bb2c8c659120757d767e6ce9c157
Eievui <14899090+eievui5@users.noreply.github.com>
2023-06-14 11:44:01 -0400

    Add get_flag unit test

--- tools/src/bin/unit-test.rs
+++ tools/src/bin/unit-test.rs
@@ -2,7 +2,78 @@ use evunit::{run_tests, read_symfile};
 use evunit::log::SilenceLevel;
 use evunit::registers::Registers;
 use evunit::test::TestConfig;
+use std::collections::HashMap;
 use std::process::exit;
+
+type Symfile = HashMap<String, (u32, u16)>;
+
+fn get_bit_a(
+	tests: &mut Vec<TestConfig>,
+	symfile: &Symfile,
+	base_test: &TestConfig,
+) {
+	let get_bit_a = symfile["GetBitA"].1;
+
+	for i in 0..8 {
+		let mut test = base_test.clone();
+		test.name = format!("GetBitA({i})");
+
+		test.initial = test.initial
+			.with_a(i)
+			.with_pc(get_bit_a);
+
+		test.result = Some(Registers::new()
+			.with_a(1 << i));
+
+		tests.push(test);
+	}
+}
+
+fn get_xp_reward(
+	tests: &mut Vec<TestConfig>,
+	symfile: &Symfile,
+	base_test: &TestConfig,
+) {
+	let get_xp_reward = symfile["GetXpReward"].1;
+
+	for i in 0..=20 {
+		let mut test = base_test.clone();
+		test.name = format!("GetXpReward({i})");
+
+		test.initial = test.initial
+			.with_a(i)
+			.with_pc(get_xp_reward);
+
+		test.result = Some(Registers::new()
+			.with_a(i * 10 + 15));
+
+		tests.push(test);
+	}
+}
+
+fn get_flag(
+	tests: &mut Vec<TestConfig>,
+	symfile: &Symfile,
+	base_test: &TestConfig,
+) {
+	let get_flag = symfile["GetFlag"].1;
+	let flag_array = symfile["wFlags"].1;
+
+	for i in 0..=255 {
+		let mut test = base_test.clone();
+		test.name = format!("GetFlag({i})");
+
+		test.initial = test.initial
+			.with_c(i)
+			.with_pc(get_flag);
+
+		test.result = Some(Registers::new()
+			.with_a(1 << (i & 0b111))
+			.with_hl(flag_array + ((i as u16) >> 3)));
+
+		tests.push(test);
+	}
+}
 
 fn main() {
 	let rom = "../bin/esprit.gb";
@@ -17,41 +88,12 @@ fn main() {
 
 	// Generate some tests!
 	let mut tests = Vec::new();
-
-	let get_bit_a = symfile["GetBitA"].1;
-
-	for i in 0..8 {
-		let mut test = base_test.clone();
-		test.name = format!("GetBitA {i}");
-
-		test.initial = test.initial
-			.with_a(i)
-			.with_pc(get_bit_a);
-
-		test.result = Some(Registers::new()
-			.with_a(1 << i));
-
-		tests.push(test);
-	}
-
-	let get_xp_reward = symfile["GetXpReward"].1;
-
-	for i in 0..=20 {
-		let mut test = base_test.clone();
-		test.name = format!("GetXpReward {i}");
-
-		test.initial = test.initial
-			.with_a(i)
-			.with_pc(get_xp_reward);
-
-		test.result = Some(Registers::new()
-			.with_a(i * 10 + 15));
-
-		tests.push(test);
-	}
+	get_bit_a(&mut tests, &symfile, &base_test);
+	get_xp_reward(&mut tests, &symfile, &base_test);
+	get_flag(&mut tests, &symfile, &base_test);
 
 	// Run and exit.
-	let result = run_tests(rom, &tests, SilenceLevel::Passing);
+	let result = run_tests(rom, &tests, SilenceLevel::None);
 
 	if result.is_err() {
 		exit(1);
